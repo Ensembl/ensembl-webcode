7eae0ca04443599479806db0bd6591bdbd05462d
Steve Trevanion <st3@sanger.ac.uk>
2007-06-15 15:23:05 +0000

    remove warn

--- modules/EnsEMBL/Web/Component/Location.pm
+++ modules/EnsEMBL/Web/Component/Location.pm
@@ -436,7 +436,7 @@ sub cytoview_config {
   my $string = $session->get_script_config_as_string($script_name);
   my $html = "";
 
-  warn "CHECKING FOR CURRENT CONFIG: " . $current_config->config;
+#  warn "CHECKING FOR CURRENT CONFIG: " . $current_config->config;
   if ($current_config) {
     foreach my $configuration (@{ $data_user->configurations }) {
       if ($configuration->id eq $current_config->config) {

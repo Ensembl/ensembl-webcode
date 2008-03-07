274dd34bf9da24598cc80b112c24fac5a0cbd30a
Steve Trevanion <st3@sanger.ac.uk>
2008-03-07 15:23:11 +0000

    vega configs - do not put on HEAD!

--- modules/EnsEMBL/Web/UserConfig/Vmap2view.pm
+++ modules/EnsEMBL/Web/UserConfig/Vmap2view.pm
@@ -5,83 +5,137 @@ use vars qw(@ISA);
 @ISA = qw(EnsEMBL::Web::UserConfig);
 
 sub init {
-  my ($self) = @_;
-  $self->{'_label'}           = 'above',
-  $self->{'_band_labels'}     = 'on',
-  $self->{'_image_height'}    = 450,
-  $self->{'_top_margin'}      = 40,
-  $self->{'_band_links'}      = 'yes',
-  $self->{'_userdatatype_ID'} = 109;
 
-  $self->{'general'}->{'Vmap2view'} = {
-    '_artefacts'   => [qw(Vsnps Vpercents Vgenes Vsupercontigs Videogram)],
-    '_options'   => [],
+    my ($self) = @_;
+    $self->{'_label'}           = 'above';
+    $self->{'_uppercase_label'} = 'no';
+    $self->{'_band_labels'}     = 'on';
+    $self->{'_image_height'}    = 450;
+    $self->{'_top_margin'}      = 40;
+    $self->{'_spacing'}		= 8; # spacing between lanes in ideogram
+    $self->{'_band_links'}      = 'yes';
+    $self->{'_userdatatype_ID'} = 109;
 
-    '_settings' => {
-      'opt_zclick'  => 1,
-      'width'       => 500, # really height <g>
-      'bgcolor'     => 'background1',
-      'bgcolour1'   => 'background1',
-      'bgcolour2'   => 'background1',
-      'labels'      => 1
-     },
-    'Vgenes' => {
-      'on'          => 'off',
-      'pos'         => '100',
-      'width'       => 60,
-      'col_genes'   => 'black',
-      'col_xref'    => 'rust',
-      'col_pred'    => 'black',
-      'col_known'   => 'rust',
-      'logicname' => 'knownGeneDensity geneDensity'
+    $self->{'general'}->{'Vmap2view'} = {
+	'_artefacts'   => [qw(
+            Vannotation_status_left
+            Vannotation_status_right
+			Vannot_TotPCod
+			Vannot_TotPTrans
+            Vannot_TotPseudo
+			Vannot_TotIgSeg
+			Vannot_TotTEC
+            Vsnps
+	        Vpercents
+    	    Videogram
+	)],
 
-    },
-    'Vrefseqs' => {
-      'on'          => 'off',
-      'pos'         => '110',
-      'width'       => 60,
-      'col'         => 'blue',
-      'logicname' => 'refseqs'
+	'_options'   => [],
 
-    },        
-    'Vpercents' => {
-      'on'          => 'off',
-      'pos'         => '200',
-      'width'       => 60,
-      'col_gc'      => 'red',
-      'col_repeat'  => 'black',
-      'logicname'   => 'PercentageRepeat PercentGC'
-    },    
-    'Vsnps' => {
-      'on'          => 'off',
-      'pos'         => '300',
-      'width'       => 60,
-      'col'         => 'blue',
-      'logicname' => 'snpDensity'
-    },        
-    'Videogram' => {
-      'on'          => "on",
-      'pos'         => '1000',
-      'width'       => 24,
-      'bandlabels'  => 'on',
-      'totalwidth'  => 100,
-      'col'         => 'g',
-      'padding'     => 6,
-    },
-    'Vsupercontigs' => {
-       'on'          => 'off',
-       'pos'         => '400',
-       'width'       =>  20,
-       'totalwidth'  =>  100,
-       'padding'     =>  6,
-       'col'         => 'blue',
-       'col_ctgs1'    => 'green',
-       'col_ctgs2'    => 'darkgreen',
-       'lab'         => 'black',
-       'available'   => 'features MAPSET_SUPERCTGS',
+	'_settings' => {
+	    'width'     => 500, # really height <g>
+	    'bgcolor'   => 'background1',
+	    'bgcolour1' => 'background1',
+	    'bgcolour2' => 'background1',
+            'scale_values' => [qw(
+				PCodDensity
+     			PTransDensity
+                PseudoGeneDensity
+			    IgSegDensity
+                TECGeneDensity
+            )],
+	    'labels' => 1,
+	},
+       
+	'_colours' => {
+	    $self->{'_colourmap'}->colourSet( 'vega_gene_havana' ),
+        },
 
-    }
+        'Vannotation_status_left' => {
+            'on'          => 'on',
+            'pos'         => '1',
+            'colour'      => 'gray85',
+            'glyphset'    => 'Vannotation_status',
+            'tag_pos'     => 1,
+        },
 
-  };
+        'Vannotation_status_right' => {
+            'on'          => 'on',
+            'pos'         => '1000',
+            'colour'      => 'gray85',
+            'glyphset'    => 'Vannotation_status',
+            'tag_pos'     => 0,
+        },
+
+  	    'Vannot_TotPCod' => {
+	        'on' => 'on',
+	        'pos' => '10',
+	        'width' => 40,
+            'glyphset' => 'Vgenedensity_vega',
+            'label' => ["Prot.Cod."],
+            'colour' => [qw(protein_coding)],
+    	    'logicname' => 'PCodDensity',
+		},
+
+	    'Vannot_TotPTrans' => {
+	        'on' => 'on',
+	        'pos' => '12',
+	        'width' => 40,
+            'glyphset' => 'Vgenedensity_vega',
+            'label' => ["Proc.Trans."],
+            'colour' => [qw(processed_transcript)],
+    	    'logicname' => 'PTransDensity',
+    	},
+
+	    'Vannot_TotPseudo' => {
+	        'on' => 'on',
+	        'pos' => '15',
+	        'width' => 40,
+            'glyphset' => 'Vgenedensity_vega',
+            'label' => ["Pseudo."],
+            'colour' => [qw(pseudogene)],
+    	    'logicname' => 'PseudoGeneDensity',
+    	},
+
+	    'Vannot_TotIgSeg' => {
+	        'on' => 'on',
+	        'pos' => '18',
+	        'width' => 40,
+            'glyphset' => 'Vgenedensity_vega',
+            'label' => ["Ig Seg."],
+            'colour' => [qw(ig_segment)],
+    	    'logicname' => 'IgSegDensity',
+    	},
+
+	    'Vannot_TotTEC' => {
+	        'on' => 'on',
+	        'pos' => '21',
+	        'width' => 40,
+            'glyphset' => 'Vgenedensity_vega',
+            'label' => ["TEC"],
+            'colour' => [qw(tec)],
+    	    'logicname' => 'TECGeneDensity',
+    	},
+
+	    'Vpercents' => {
+	        'on' => 'on',
+	        'pos' => '99',
+	        'width' => 30,
+	        'col_gc' => 'red',
+	        'col_repeat' => 'black',
+	        'logicname' => 'PercentageRepeat PercentGC'
+	    },		
+
+	    'Videogram' => {
+	        'on'  => "on",
+	        'pos' => '100',
+	        'width' => 24,
+	        'bandlabels' => 'on',
+	        'totalwidth' => 100,
+	        'col' => 'g',
+	        'padding'   => 6,
+     	},
+    };
 }
 1;
+

89e942f7f5973069d141676149c1bcdf83dedb28
Stephen Keenan <keenan@ebi.ac.uk>
2008-06-06 13:23:56 +0000

    *** empty log message ***

--- modules/EBeyeSearch/Renderer.pm
+++ modules/EBeyeSearch/Renderer.pm
@@ -37,17 +37,15 @@ sub render_summary() {
   return $self->_render_text( "Enter the string you wish to search for in the box above." ) unless $self->ebeye->query;
 
   my $pager = $self->ebeye->pager;
-my $total_entries = $pager->total_entries;
-my $page_first_hit = $pager->first;
-my $page_last_hit = $pager->last;
+  my $total_entries = $pager->total_entries;
+  my $page_first_hit = $pager->first;
+  my $page_last_hit = $pager->last;
+  my $query = $self->ebeye->query;
 
-
-  if( $total_entries > 10 ) {
-    return $self->_render_text( "Your query matched $total_entries entries in the search database. Viewing hits $page_first_hit-$page_last_hit" );
-  } elsif( $total_entries > 0 ) {
-    return $self->_render_text( "Your query matched $total_entries entries in the search database" );
+  if( $total_entries > 0 ) {
+      return $self->_render_text( qq{Results <strong>$page_first_hit-$page_last_hit</strong> of <strong>$total_entries</strong> for <strong>$query.</strong>});
   } else {
-    return $self->_render_text( "Your query matched no entries in the search database" );
+    return $self->_render_text( qq{Your query <strong>- $query -</strong> did not match any records in the database. Please make sure all terms are spelled correctly}  );
   }
 }
 
@@ -57,21 +55,31 @@ sub render_pagination {
   my $self = shift;
   return unless my $query = $self->ebeye->query;
   return if $self->ebeye->nhits < 11;
-  my $current_page = $self->ebeye->pager->current_page;
-  my $last_page = $self->ebeye->pager->last_page;
+  my $pager = $self->ebeye->pager;
+  my $current_page = $pager->current_page;
+  my $last_page = $pager->last_page;
+  my $previous_page = $pager->previous_page;
+  my $next_page = $pager->next_page;
 
   my $out = '<div class="paginate">';
+
+  if ( $pager->previous_page) {
+      $out .= sprintf( '<a href="?ebi_search;q=%s;page=%s">prev</a> ', $query ,$pager->previous_page );
+  }
   foreach my $i (1..$last_page) {
-    if( $i == $current_page ) {
-      $out .= sprintf( '<strong>%s</strong> ', $i );
-    } elsif( $i < 5 || ($last_page-$i)<4 || abs($i-$current_page+1)<4 ) {
-#       my $T = new ExaLead::Link( "_s=".(($i-1)*10), $self->ebeye );
-      $out .= sprintf( '<a href="?ebi_search;q=%s;page=%s">%s</a> ', $query ,$i, $i );
-    } else {
-      $out .= '..';
-    }
+      if( $i == $current_page ) {
+	  $out .= sprintf( '<strong>%s</strong> ', $i );
+      } elsif( $i < 5 || ($last_page-$i)<4 || abs($i-$current_page+1)<4 ) {
+	  #       my $T = new ExaLead::Link( "_s=".(($i-1)*10), $self->ebeye );
+	  $out .= sprintf( '<a href="?ebi_search;q=%s;page=%s">%s</a> ', $query ,$i, $i );
+      } else {
+	  $out .= '..';
+      }
   }
   $out =~ s/\.\.+/ ... /g;
+  if ($pager->next_page) {
+      $out .= sprintf( '<a href="?ebi_search;q=%s;page=%s">next</a> ', $query ,$pager->next_page );
+  }  
   return "$out</div>";
 }
 
@@ -84,6 +92,20 @@ sub _render_text {
   my( $self, $text ) = @_;
   return "<p>$text</p>";
 }
+
+
+sub render_form {
+  my $self = shift;
+
+
+  return qq(
+    <form action="" method="get" style="margin: 3px 1em;" >
+      <input type="text" name="q" value="" style="width: 300px" />
+        <input type="submit" value="Search" />
+    </form>
+  );
+}
+
 
 
 

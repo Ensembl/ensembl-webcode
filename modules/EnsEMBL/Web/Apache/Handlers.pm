3924cc3785c59aaf8a0edf16c36624156b536ab7
Evgeny Bragin <eb4@sanger.ac.uk>
2008-11-28 17:27:31 +0000

    *** empty log message ***

--- modules/EnsEMBL/Web/Apache/Handlers.pm
+++ modules/EnsEMBL/Web/Apache/Handlers.pm
@@ -439,14 +439,16 @@ sub cleanURI {
   
   ## Clean out the uri
   my $uri = $ENV{'REQUEST_URI'};
-  if ($uri =~ s/time=\d+\.\d+;?//g) {
+  if ($uri =~ s/time=\d+\.\d+;?//g + $uri =~ s!([^:])/{2,}!$1/!g) {
+    warn "uri = $uri";
     $r->parse_uri($uri);
     $r->subprocess_env('REQUEST_URI' => $uri);
   }
 
   ## Clean out the referrer
   my $referer = $ENV{'HTTP_REFERER'};
-  if ($referer =~ s/time=\d+\.\d+;?//g) {
+  if ($referer =~ s/time=\d+\.\d+;?//g + $referer =~ s!([^:])/{2,}!$1/!g) {
+    warn "ref = $referer";
     $r->subprocess_env('HTTP_REFERER' => $referer);
   }
 

f14ad3a72a4dd9a3d8f46eac7ac6eeb2c36cad79
Web Admin <w3adm@sanger.ac.uk>
2009-05-07 15:46:41 +0000

    added zmenu for Supercontigs on vertical Chromosome Image

--- modules/EnsEMBL/Web/Configuration/Location.pm
+++ modules/EnsEMBL/Web/Configuration/Location.pm
@@ -361,7 +361,12 @@ sub ajax_zmenu      {
   }
   elsif ($action =~ /View|Overview/) {
     return $self->_ajax_zmenu_view($panel,$obj);
+  }  
+  elsif ($action =~ /Supercontigs/) {
+    return $self->_ajax_zmenu_supercontig($panel,$obj);
   }
+
+
   return;
 }
 
@@ -391,6 +396,22 @@ sub _ajax_zmenu_view {
 	'action' => $action});
     $panel->add_entry({ 'label' => $r, 'link'  => $url });
   }
+}
+
+sub _ajax_zmenu_supercontig {
+  my $self  = shift;
+  my $panel = shift;
+  my $obj   = shift;
+    
+  $panel->{'caption'} = $obj->param('ctg') ." " . $obj->param('r');
+  my $url = $obj->_url({
+    'type'      => 'Location',
+    'action'    => 'Overview',
+    'r'         => $obj->param('r'),
+    'cytoview'  => 'misc_feature_core_superctgs=normal'
+  });
+  $panel->add_entry({'label' => 'Jump to Supercontig', 'link' => $url}); 
+  return;
 }
 
 sub _ajax_zmenu_synteny {

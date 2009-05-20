797ab2bbec8116e431314a7a4b3e5bcca9f6ecd9
Anne Parker <ap5@sanger.ac.uk>
2009-05-20 08:26:10 +0000

    Fixed problem with access to UseBookmark from group records

--- modules/EnsEMBL/Web/Filter/Owner.pm
+++ modules/EnsEMBL/Web/Filter/Owner.pm
@@ -13,15 +13,10 @@ use base qw(EnsEMBL::Web::Filter);
 sub BUILD {
   my ($self, $ident, $args) = @_;
   $self->set_redirect('/Account/Links');
-  ## Standard exceptions to prevent filter being used on new records,
-  ## which perforce have no owner yet!
-  $self->set_exceptions({
-    'list'  => ['Add', 'Preview', 'Save'],
-    'param' => 'id',
-  });
   ## Set the messages hash here
   $self->set_messages({
     'not_owner' => 'You are not the owner of this record.',
+    'not_member' => 'You are not a member of the group that owns this record.',
     'bogus_id' => 'No valid record selected.',
   });
 }
@@ -30,22 +25,42 @@ sub BUILD {
 sub catch {
   my $self = shift;
   my $object = $self->object;
-  ## First check we have a sensible value for 'id'
-  if ($object->param('id') && $object->param('id') =~ /\D/) {
-    $self->set_error_code('bogus_id');
-    return;
-  }
-  ## Ignore exception actions
-  my %exceptions = %{$self->get_exceptions};
-  my $ignore = grep {$_ eq $ENV{'ENSEMBL_FUNCTION'}} @{$exceptions{'list'}};
-
-  unless ($ignore) {
-    my $user = $ENSEMBL_WEB_REGISTRY->get_user;
-    my $type = $object->param('type') || $ENV{'ENSEMBL_ACTION'};
-#    my $method = lc($type).'s';
-    my $record = $user->records($object->param('id')); 
-    unless ($record) {
-      $self->set_error_code('not_owner');
+  ## Don't fail if no ID - implies new record
+  if ($object->param('id')) {
+    ## First check we have a sensible value for 'id'
+    if ($object->param('id') =~ /\D/) {
+      $self->set_error_code('bogus_id');
+      return;
+    }
+    else {
+      my $user = $ENSEMBL_WEB_REGISTRY->get_user;
+      if ($object->param('group')) {
+        ## First check we have a sensible value for 'id'
+        if ($object->param('group') =~ /\D/) {
+          $self->set_error_code('bogus_id');
+          return;
+        }
+        else {
+          my $group = EnsEMBL::Web::Data::Group->new($object->param('group'));
+          if ($group && $user->is_member_of($group)) {
+            my $record = $group->records($object->param('id')); 
+            unless ($record) {
+              $self->set_error_code('not_owner');
+              return;
+            }
+          }
+          else {
+            $self->set_error_code('not_member');
+            return;
+          }
+        }
+      }
+      else {
+        my $record = $user->records($object->param('id')); 
+        unless ($record) {
+          $self->set_error_code('not_owner');
+        }
+      }
     }
   }
 }

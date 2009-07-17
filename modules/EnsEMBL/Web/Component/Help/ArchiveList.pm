7c882f4b3d35cfb2b0ab44019f7d9f35cfdd7ff9
Web Admin <w3adm@sanger.ac.uk>
2009-07-17 15:51:55 +0000

    list of archive pages for species pages

--- modules/EnsEMBL/Web/Component/Help/ArchiveList.pm
+++ modules/EnsEMBL/Web/Component/Help/ArchiveList.pm
@@ -49,11 +49,25 @@ sub content {
       $html .= "<p>The following archives are available for this page:</p>
 <ul>\n";
       my $missing = 0;
+      #can this go ?
       if ($type =~ /\.html/ || $action =~ /\.html/) {
         foreach my $release (reverse sort keys %archive) {
-          next if $release == $object->species_defs->VERSION;
+          next if $release == $object->species_defs->ENSEMBL_VERSION;
           $html .= $self->_output_link(\%archive, $release, $url);
         }
+      }
+      #species home pages
+      if ($type eq 'Info' && $action eq 'Index') {
+	foreach my $release (reverse sort keys %archive) {
+	  next if $release == $object->species_defs->ENSEMBL_VERSION;
+	  if ($release > 50) {
+	    $html .= $self->_output_link(\%archive, $release, $url);
+	  }
+	  else {
+	    $url =~ s/Info\/Index/index\.html/;
+	    $html .= $self->_output_link(\%archive, $release, $url);
+	  }
+	}
       }
       else {
 	my $releases = EnsEMBL::Web::OldLinks::get_archive_redirect($type, $action, $object);

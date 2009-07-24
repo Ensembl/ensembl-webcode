de7cefcb347d337351eeca72311c33955b3bc73a
Web Admin <w3adm@sanger.ac.uk>
2009-07-24 12:41:51 +0000

    fixed display of ensembl/havana merged track display in config panel

--- modules/EnsEMBL/Web/ImageConfig/gene_summary.pm
+++ modules/EnsEMBL/Web/ImageConfig/gene_summary.pm
@@ -51,6 +51,10 @@ sub init {
     [qw(transcript prediction variation)],
     {qw(display off)} 
   );
+  $self->modify_configs(
+    [qw(transcript_core)],
+    {qw(display transcript_label)}
+  );
 
 }
 1;

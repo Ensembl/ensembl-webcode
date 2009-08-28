57adae81a9683fe6a238dda86456498e9ec5b08f
Eugene Kulesha <ek@ebi.ac.uk>
2009-08-28 16:22:02 +0000

    added adaptor for pan compara

--- modules/EnsEMBL/Web/Tools/Registry.pm
+++ modules/EnsEMBL/Web/Tools/Registry.pm
@@ -27,8 +27,8 @@ sub configure {
     'VEGA_ENSEMBL'     => 'Bio::EnsEMBL::DBSQL::DBAdaptor',
     'CORE'             => 'Bio::EnsEMBL::DBSQL::DBAdaptor',
     'COMPARA'          => 'Bio::EnsEMBL::Compara::DBSQL::DBAdaptor',
-    'COMPARA_PAN_ENSEMBL' => 'Bio::EnsEMBL::Compara::DBSQL::DBAdaptor',
     'USERDATA'         => 'Bio::EnsEMBL::DBSQL::DBAdaptor',
+    'COMPARA_PAN_ENSEMBL' => 'Bio::EnsEMBL::Compara::DBSQL::DBAdaptor',
     'COMPARA_MULTIPLE' => undef,
     'WEBSITE'          => undef,
     'HEALTHCHECK'      => undef,
@@ -37,6 +37,7 @@ sub configure {
     'MART'             => undef,
     'GO'               => undef,
     'FASTA'            => undef,
+
   );
 
   for my $species ( keys %{$self->{'conf'}->{_storage}},'MULTI' ) {

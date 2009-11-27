a4b7650fe7b7f8f0552959c03b458deef3ee32fe
cvs2git <cvs2git>
2009-11-27 14:39:08 +0000

    This commit was manufactured by cvs2svn to create branch 'branch- ensembl-56'.
    Cherrypick from master 2009-11-26 16:56:32 UTC Beth Yates <bp1@sanger.ac.uk> 'new code for Variation consequence calculator tool':
        modules/EnsEMBL/Web/Command/UserData/SNPConsequence.pm
        modules/EnsEMBL/Web/Component/Location/MultiBottomNav.pm
        modules/EnsEMBL/Web/Component/UserData/UploadVariations.pm
        modules/EnsEMBL/Web/Text/Feature/CONSEQUENCE.pm
    Cherrypick from master 2009-11-27 14:39:07 UTC Beth Yates <bp1@sanger.ac.uk> 'check if we have an error in the data':
        modules/EnsEMBL/Web/Component/UserData/ConsequenceTool.pm

---
+++ modules/EnsEMBL/Web/Component/UserData/ConsequenceTool.pm
@@ -0,0 +1,49 @@
+package EnsEMBL::Web::Component::UserData::ConsequenceTool;
+
+use strict;
+use warnings;
+no warnings "uninitialized";
+use base qw(EnsEMBL::Web::Component::UserData);
+use CGI qw(escapeHTML);
+
+sub _init {
+  my $self = shift;
+  $self->cacheable( 0 );
+  $self->ajaxable(  1 );
+}
+
+sub caption {
+  return;
+}
+
+sub content {
+  my $self = shift;
+  my $object = $self->object;
+  my $html = '<h2>Consequence Calculator Results:</h2>';
+  my $referer =  $object->param('_referer');
+  $html .= qq(<br /><a href="$referer">Back to previous view</a><br />);
+
+
+  my @files = ($object->param('convert_file'));
+
+  foreach my $file_name (@files) {
+    my ($file, $name) = split(':', $file_name);  
+    my ($table, $error) = $object->calculate_consequence_data($file);
+    if ($error) {  return $self->error($table); }
+    $html .= $table->render;
+  }
+
+  return $html;
+}
+
+sub error {
+  my ($self, $error_text) = (@_);
+  my $html = $self->_info(
+    'Error Parsing the data',
+    $error_text
+  );
+  return $html;
+
+}
+
+1;

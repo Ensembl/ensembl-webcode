a7dcca73887228b41ef2470b614202a054e2c27b
Web Group <www-ens@sanger.ac.uk>
2010-03-31 16:13:26 +0000

    updated help text

--- modules/EnsEMBL/Web/Command/UserData/SNPConsequence.pm
+++ modules/EnsEMBL/Web/Command/UserData/SNPConsequence.pm
@@ -28,7 +28,15 @@ sub process {
     my ($table, $file_count) = $object->calculate_consequence_data($file, $size_limit);
     
     if ($file_count){
-      $output .= 'Your file contained '.$file_count .' features however this web tool will only convert the first '. $size_limit .' features in the file.'."\n\n";
+      $output .= 'Your file contained '.$file_count .' features however this web tool will only convert 
+              the first '. $size_limit .' features in the file. There is a limit of $variation_limit variations 
+              that can be processed at any one time.
+              You can upload a file that contains more entries, however anything after the $variation_limit
+              line will be ignored. If your file contains more than $variation_limit variations you can split
+              your file into smaller chunks and process them one at a time, or you may wish to use the variation API 
+              or a standalone perl script available on the Ensembl FTP site 
+              ftp://ftp.ensembl.org/pub/misc-scripts/SNP_effect_predictor_1.0 perl which you can run on your own 
+              machine to generate the same results as this web tool.'."\n\n";
       $output .= $table->render_Text;
     } else {
      $output .= $table->render_Text;

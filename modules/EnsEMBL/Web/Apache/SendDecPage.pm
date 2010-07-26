ca286ab9560a9fc9c2ad6e861d662e2233ebe36e
Anne Parker <ap5@sanger.ac.uk>
2010-07-26 12:59:06 +0000

    Fixed bug - call to non-existent method 'add' on Document::HTML::Stylesheet

--- modules/EnsEMBL/Web/Apache/SendDecPage.pm
+++ modules/EnsEMBL/Web/Apache/SendDecPage.pm
@@ -146,7 +146,7 @@ sub handler {
       if($attr =~ /src="(.*?)"/ ) {
         $page->stylesheet->add_sheet( $media, $1 );
       } else {
-        $page->stylesheet->add( $media, $cont );
+        $page->stylesheet->add_sheet( $media, $cont );
       }
     }
   

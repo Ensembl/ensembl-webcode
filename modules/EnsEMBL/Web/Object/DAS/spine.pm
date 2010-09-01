1799997cf2205fdffc2ec9029d0ad58d4266a99c
Eugene Kulesha <ek@ebi.ac.uk>
2010-09-01 15:02:15 +0000

    temp fix - need to decide what to do with hrefs in analysis description

--- modules/EnsEMBL/Web/Object/DAS/spine.pm
+++ modules/EnsEMBL/Web/Object/DAS/spine.pm
@@ -153,7 +153,7 @@ sub Features {
 	  push @$notes, sprintf ("%s has %d transcripts containing a total of %d exons on the %s strand.", $gene_name, scalar(@{ $gene->get_all_Transcripts }),  scalar(@{ $gene->get_all_Exons }), $gene->strand > 0 ? 'forward' : 'reverse' );
 
 	  if ($gene->can('analysis') && $gene->analysis && $gene->analysis->description) {
-	      push @$notes, $gene->analysis->description;
+#	      push @$notes, $gene->analysis->description;
 	  }
 
 	  my $s1 = {

b78d50485f408045bf31f5971b4f0e536e7e1292
Eugene Kulesha <ek@ebi.ac.uk>
2010-11-01 16:38:31 +0000

    pass a known type

--- modules/EnsEMBL/Web/Apache/DasHandler.pm
+++ modules/EnsEMBL/Web/Apache/DasHandler.pm
@@ -51,7 +51,7 @@ sub handler_das {
     # this a site-wide request - try to figure out the species from the ID
     $das_species = '';
     if ($querystring =~ /segment=([^;]+)\;?(.+)?$/) {
-      my ($s) = Bio::EnsEMBL::Registry->get_species_and_object_type($1);
+      my ($s) = Bio::EnsEMBL::Registry->get_species_and_object_type($1, 'Gene');
       $das_species = $s if $s;
     }
     

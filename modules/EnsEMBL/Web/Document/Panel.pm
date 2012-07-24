6b89b1c76f4244d924accadbf815f6c02e15992a
Harpreet Riat <hr5@sanger.ac.uk>
2012-07-24 16:15:00 +0000

    divs can not go inside an 'a' tag (branch merge)

--- modules/EnsEMBL/Web/Document/Panel.pm
+++ modules/EnsEMBL/Web/Document/Panel.pm
@@ -293,11 +293,11 @@ sub _caption_h1 {
 
 sub _caption_h2_with_helplink {
   my $self  = shift;
-  my $id    = $self->{'help'};
+    my $id    = $self->{'help'};
   my $html  = '<h1 class="caption">';
      $html .= sprintf ' <a href="/Help/View?id=%s" class="popup constant help-header" title="Click for help (opens in new window)">', encode_entities($id) if $id;
      $html .= $self->{'caption'};
-     $html .= ' <div class="sprite info_icon">&nbsp;</div></a>' if $id;
+     $html .= ' <span class="sprite info_icon">&nbsp;</span></a>' if $id;
      $html .= '</h1>';
 
   return $html;

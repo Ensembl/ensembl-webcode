f158fea5c0ee9d5afa7dbcba7b8fad7741db0ec4
Dan Sheppard <ds23@sanger.ac.uk>
2012-07-24 09:56:49 +0000

    Better fix for previous commit (fewer log warnings):
    """
    Allow export of Feature::ID. Doesn't do much useful but a stack trace
    isn't the right answer, particularly if you've just selected "all" in
    the share selector, as you don't get your other data either.
    """

--- modules/EnsEMBL/Web/Text/Feature.pm
+++ modules/EnsEMBL/Web/Text/Feature.pm
@@ -8,6 +8,7 @@ sub new {
   return bless {'__raw__'=>$_[1],'__extra__'=>{} },$_[0];
 }
 
+sub _seqname { return ''; } # Override me
 sub seqname { my $self = shift; (my $T = $self->_seqname) =~s/^chr//; return $T; }
 sub start   { my $self = shift; return $self->{'start'}; }
 sub end     { my $self = shift; return $self->{'end'}; }

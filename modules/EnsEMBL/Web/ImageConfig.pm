31ef03b30fae14650045e184fbe191733ab64b18
Beth Yates <bp1@sanger.ac.uk>
2012-08-01 09:07:51 +0000

    fix rendering of Primary assembly mapping tracks in region comparison view

--- modules/EnsEMBL/Web/ImageConfig.pm
+++ modules/EnsEMBL/Web/ImageConfig.pm
@@ -1462,8 +1462,7 @@ sub add_dna_align_features {
       my $display = (grep { $data->{$key_2}{'display'} eq $_ } @{$self->{'alignment_renderers'}}) ? $data->{$key_2}{'display'} : 'off'; # needed because the same logic_name can be a gene and an alignment
       my $glyphset = '_alignment';
       my $strand = 'b';
-
-      if ($data->{$key_2}{'display'} && $data->{$key_2}{'display'} eq 'simple'){
+      if ($key_2 eq 'alt_seq_mapping'){
         $display = 'simple';
         $alignment_renderers = ['off', 'Off', 'normal', 'On'];  
         $glyphset = 'patch_ref_alignment';

56d0c1d750425ddb7d1fa0f83b530edfb81ea98d
Dan Sheppard <ds23@sanger.ac.uk>
2012-11-08 09:52:27 +0000

    Force ROSE and Class::DBI to use different pools. Don't allow parent/child connection sharing in ROSE to avoid premature cleanup.
    Two further attempts to rectify admin lost connection issue. Improvements,
    but are they the current issue?

--- modules/EnsEMBL/Web/DBSQL/DirectDBConnection.pm
+++ modules/EnsEMBL/Web/DBSQL/DirectDBConnection.pm
@@ -14,13 +14,13 @@ sub direct_connection {
     my $dbh = $handles{$caller};
     return $dbh if $dbh and $dbh->ping;
     my $dsn = join(':','dbi','mysql',$name,$host,$port);
-    $dbh = DBI->connect_cached($dsn,$user,$pass,
+    $dbh = DBI->connect($dsn,$user,$pass,
       {
         $caller->_default_attributes,
         RaiseError => 1,
         PrintError => 1,
         AutoCommit => 1,
-        mysql_connect_timeout => 30,
+        mysql_connect_timeout => 23,
       }
     ) || die "Can not connect to $dsn";
     EnsEMBL::Web::Controller->disconnect_on_request_finish($dbh);

eac311babfe4f6ac00c77025a4c2653917b86b34
cvs2git <cvs2git>
2013-03-01 09:56:13 +0000

    This commit was manufactured by cvs2svn to create branch 'branch- ensembl-70'.
    Cherrypick from master 2013-02-05 15:12:25 UTC Simon Brent <sb23@sanger.ac.uk> 'don't cache cell_line config - it takes no time at all, and the spacers change depending on image width and which tracks are one':
        htdocs/components/02_PanelManager.js
        modules/EnsEMBL/Web/ImageConfig/regulation_view.pm
    Cherrypick from master 2013-03-01 09:56:11 UTC Simon Brent <sb23@sanger.ac.uk> 'wow, that was stupid':
        modules/EnsEMBL/Web/ImageConfig/Vertical.pm

---
+++ modules/EnsEMBL/Web/ImageConfig/Vertical.pm
@@ -0,0 +1,255 @@
+# $Id$
+
+package EnsEMBL::Web::ImageConfig::Vertical;
+
+## Alternative configuration for karyotype used in BlastView
+use strict;
+
+use EnsEMBL::Web::Text::FeatureParser;
+use EnsEMBL::Web::TmpFile::Text;
+use EnsEMBL::Web::Tools::Misc;
+
+use base qw(EnsEMBL::Web::ImageConfig);
+
+# We load less data on vertical drawing code, as it shows regions 
+# at a much smaller scale. We also need to distinguish between
+# density features, rendered as separate tracks, and pointers,
+# which are part of the karyotype track
+sub load_user_tracks {
+  my ($self, $session) = @_;
+  my $menu = $self->get_node('user_data');
+  
+  return unless $menu;
+  
+  $self->SUPER::load_user_tracks($session);
+  
+  my $width              = $self->get_parameter('all_chromosomes') eq 'yes' ? 10 : 60;
+  my %remote_formats     = map { lc $_ => 1 } @{$self->hub->species_defs->USERDATA_REMOTE_FORMATS};
+  my (undef, $renderers) = $self->_user_track_settings;
+  
+  foreach ($menu->nodes) {
+    if ($remote_formats{lc $_->get('format')}
+      || ($_->get('format') eq 'report' || $_->get('name') eq 'Region report')
+      ) {
+      $_->remove;
+    } else {
+      $_->set('renderers', $renderers);
+      $_->set('glyphset',  'Vuserdata');
+      $_->set('colourset', 'densities');
+      $_->set('maxmin',    1);
+      $_->set('width',     $width);
+      $_->set('strand',    'b');
+    }
+  }
+}
+
+sub _user_track_settings {
+  return ('b', [
+    'off',                'Off',
+    'highlight_lharrow',  'Arrow on lefthand side',
+    'highlight_rharrow',  'Arrow on righthand side',
+    'highlight_bowtie',   'Arrows on both sides',
+    'highlight_wideline', 'Line',
+    'highlight_widebox',  'Box',
+    'density_line',       'Density plot - line graph',
+    'density_bar',        'Density plot - filled bar chart',
+    'density_outline',    'Density plot - outline bar chart',
+  ]);
+}
+
+sub load_user_track_data {
+  my ($self, $chromosomes) = @_;
+  my $hub          = $self->hub;
+  my $species_defs = $hub->species_defs;
+  my $bins         = 150;
+  my $bin_size     = int($self->container_width / $bins);
+  my $track_width  = $self->get_parameter('width') || 80;
+  my @colours      = qw(darkred darkblue darkgreen purple grey red blue green orange brown magenta violet darkgrey);
+  my ($feature_adaptor, $slice_adaptor, $parser, %data, $max);
+  
+  foreach my $track ($self->get_node('user_data')->nodes) {
+    my $display = $track->get('display');
+    my $ftype = $track->get('ftype');
+    
+    next if $display eq 'off';
+    
+    my $logic_name = $track->get('logic_name');
+    my $colour     = \@colours;
+    my $max_value;
+    
+    unshift @$colour, 'black' if $display eq 'density_graph'; 
+    
+    if ($logic_name) {
+      $feature_adaptor ||= $hub->get_adaptor('get_DnaAlignFeatureAdaptor', 'userdata');
+      $slice_adaptor   ||= $hub->get_adaptor('get_SliceAdaptor');
+      
+      ($data{$track->id}, $max_value) = $self->get_dna_align_features($logic_name, $feature_adaptor, $slice_adaptor, $bins, $bin_size, $chromosomes, $colour->[0]);
+    } else {
+      if ($parser) {
+        $parser->reset;
+      } else {
+        $parser = EnsEMBL::Web::Text::FeatureParser->new($species_defs);
+        $parser->no_of_bins($bins);
+        $parser->bin_size($bin_size);
+        $parser->filter($chromosomes->[0]) if scalar @$chromosomes == 1;
+      }
+      
+      ($data{$track->id}, $max_value) = $self->get_parsed_features($track, $parser, $bins, $colour);
+    }
+    
+    $max = $max_value if $max_value > $max;
+  }
+  
+  if ($max) {
+    my $scale = $track_width / $max;
+    
+    foreach my $id (keys %data) {
+      foreach my $chr (keys %{$data{$id}}) {
+        foreach my $track_data (values %{$data{$id}{$chr}}) {
+          $_ *= $scale for @{$track_data->{'scores'} || []};
+          $self->get_node($id)->set('colour', $track_data->{'colour'});
+        }
+      }
+    }
+  }
+  
+  $self->set_parameter('bins',      $bins);
+  $self->set_parameter('max_value', $max);
+  
+  return \%data;
+}
+
+sub get_dna_align_features {
+  my ($self, $logic_name, $feature_adaptor, $slice_adaptor, $bins, $bin_size, $chromosomes, $colour) = @_;
+  my (%data, $max);
+  
+  foreach my $chr (@$chromosomes) {
+    my $slice    = $slice_adaptor->fetch_by_region('chromosome', $chr);
+    my $features = $feature_adaptor->fetch_all_by_Slice($slice, $logic_name);
+    
+    next unless scalar @$features;
+    
+    my $start = 1;
+    my $end   = $bin_size;
+    my @scores;
+    
+    for (0..$bins-1) {
+      my $count    = scalar grep { $_->start >= $start && $_->end <= $end } @$features;
+      
+      $_  += $bin_size for $start, $end;
+      $max = $count if $max < $count;
+      
+      push @scores, $count;
+    }
+    
+    $data{$chr}{'dnaAlignFeature'} = {
+      scores => \@scores,
+      colour => $colour,
+      sort   => 0,
+    };
+  }
+  
+  return (\%data, $max);
+}
+
+sub get_parsed_features {
+  my ($self, $track, $parser, $bins, $colours) = @_;
+  my $url     = $track->get('url');
+  my $content = $url ? EnsEMBL::Web::Tools::Misc::get_url_content($url)->{'content'} : EnsEMBL::Web::TmpFile::Text->new(filename => $track->get('file'))->retrieve;
+  
+  return undef unless $content;
+  
+  $parser->parse($content, $track->get('format'));
+  
+  my $max_values = $parser->max_values;
+  my $sort       = 0;
+  my (%data, $max);
+  
+  while (my ($name, $track_data) = each %{$parser->get_all_tracks}) {
+    my $count;
+    
+    while (my ($chr, $results) = each %{$track_data->{'bins'}}) {
+      $data{$chr}{$name} = {
+        scores => [ map $results->[$_], 0..$bins-1 ],
+        colour => $track_data->{'config'}{'color'} || $colours->[$count],
+        sort   => $sort
+      };
+    }
+    
+    $max = $max_values->{$name} if $max < $max_values->{$name};
+    
+    $count++ unless $track->{'config'}{'color'};
+    $sort++;
+  }
+  
+  return (\%data, $max);
+}
+
+sub create_user_features {
+  my $self   = shift;
+  my $hub    = $self->hub;
+  my $menu   = $self->get_node('user_data');
+  my $tracks = {};
+  
+  return $tracks unless $menu;
+  
+  foreach ($menu->nodes) {
+    next unless $_->get('display') ne 'off'; 
+    my $data   = $hub->fetch_userdata_by_id($_->id);
+    my $parser = $data->{'parser'};
+    
+    if ($parser) {
+      while (my ($type, $track) = each %{$parser->get_all_tracks}) {
+        my @rows;
+        
+        foreach my $feature (@{$track->{'features'}}) {
+          push @rows, {
+            chr     => $feature->seqname || $feature->slice->name,
+            start   => $feature->rawstart,
+            end     => $feature->rawend,
+            label   => $feature->id,
+            gene_id => $feature->id,
+            %{$feature->attribs},
+          };
+        }
+        
+        $track->{'config'}{'name'} = $data->{'name'};
+        $track->{'config'}{'ftype'} = $_->get('ftype');
+        
+        $tracks->{$_->id}{$type} = {
+          features => \@rows,
+          config   => $track->{'config'}
+        };
+      }
+    } else {
+      while (my ($analysis, $track) = each %$data) {
+        my @rows;
+       
+        foreach my $f (
+          map  { $_->[0] }
+          sort { $a->[1] <=> $b->[1] || $a->[2] cmp $b->[2] || $a->[3] <=> $b->[3] }
+          map  {[ $_, $_->{'slice'}->seq_region_name, $_->{'start'}, $_->{'end'} ]}
+          @{$track->{'features'}}
+        ) {
+          push @rows, {
+            chr     => $f->{'slice'}->seq_region_name,
+            start   => $f->{'start'},
+            end     => $f->{'end'},
+            length  => $f->{'length'},
+            label   => "$f->{'start'}-$f->{'end'}",
+            gene_id => $f->{'gene_id'},
+          };
+        }
+        
+        $tracks->{$_->id}{$analysis} = {
+          features => \@rows,
+          config   => $track->{'config'}
+        };
+      }
+    }
+  }
+  
+  return $tracks;
+}
+
+1;
---
+++ modules/EnsEMBL/Web/ImageConfig/regulation_view.pm
@@ -0,0 +1,136 @@
+# $Id$
+
+package EnsEMBL::Web::ImageConfig::regulation_view;
+
+use strict;
+
+use base qw(EnsEMBL::Web::ImageConfig);
+
+sub cache_key        { return $_[0]->code eq 'cell_line' ? '' : $_[0]->SUPER::cache_key; }
+sub load_user_tracks { return $_[0]->SUPER::load_user_tracks($_[1]) unless $_[0]->code eq 'set_evidence_types'; } # Stops unwanted cache tags being added for the main page (not the component)
+
+sub init {
+  my $self         = shift;
+  my @feature_sets = ('cisRED', 'VISTA', 'miRanda', 'NestedMICA', 'REDfly CRM', 'REDfly TFBS', 'search');
+  my @cell_lines   = sort keys %{$self->species_defs->databases->{'DATABASE_FUNCGEN'}{'tables'}{'cell_type'}{'ids'}};
+  
+  s/\:\d*// for @cell_lines;
+  
+  $self->set_parameters({
+    opt_lines => 1
+  });
+  
+  $self->create_menus(qw(
+    sequence
+    transcript
+    prediction
+    dna_align_rna
+    simple
+    misc_feature    
+    functional
+    multiple_align
+    conservation
+    variation
+    oligo
+    repeat
+    other
+    information
+  ));
+  
+  $self->load_tracks;
+  $self->load_configured_das('functional');
+  
+  $self->add_tracks('sequence',
+    [ 'contig', 'Contigs', 'contig', { display => 'normal', strand => 'r' }]
+  );
+  
+  $self->modify_configs(
+    [ 'transcript_core_ensembl' ],
+    { display => 'collapsed_nolabel' }
+  );
+  
+  $self->modify_configs(
+    [ 'gene_legend', 'variation_legend' ],
+    { display => 'off', menu => 'no' }
+  );
+  
+  $self->modify_configs(
+    [ map "regulatory_regions_funcgen_$_", @feature_sets ],
+    { menu => 'yes' }
+  );
+
+  $self->get_node('opt_empty_tracks')->set('display', 'normal');	
+
+  foreach my $cell_line (@cell_lines) {
+    $_->set('display', 'normal') for map $self->get_node("${_}_$cell_line") || (), 'reg_feats', 'seg';
+    
+    # Turn on core evidence track
+    $self->modify_configs(
+      [ "reg_feats_core_$cell_line" ],
+      { display => $cell_line =~ /^(MultiCell|CD4)$/ ? 'tiling_feature' : 'compact' }
+    );
+   
+    # Turn on supporting evidence track
+    $self->modify_configs(
+      [ "reg_feats_non_core_$cell_line" ],
+      { display => 'compact' }
+    );
+    
+    $self->{'reg_feats_tracks'}{$_} = 1 for "reg_feats_$cell_line", "reg_feats_core_$cell_line", "reg_feats_non_core_$cell_line", "seg_$cell_line";
+  }
+
+  if ($self->{'code'} ne $self->{'type'}) {    
+    my $func = "init_$self->{'code'}";
+    $self->$func if $self->can($func);
+  }  
+}
+
+sub init_top {
+  my $self = shift;
+
+  $self->add_tracks('other',
+    [ 'scalebar',                 '', 'scalebar',                 { display => 'normal', strand => 'f', menu => 'no', name => 'Scale bar' }],
+    [ 'ruler',                    '', 'ruler',                    { display => 'normal', strand => 'f', menu => 'no', name => 'Ruler'     }],
+    [ 'fg_background_regulation', '', 'fg_background_regulation', { display => 'normal', strand => 'r', menu => 'no', tag => 0            }],
+  );
+  
+  $_->remove for map $self->get_node($_) || (), keys %{$self->{'reg_feats_tracks'}};
+  $_->remove for grep $_->id =~ /_legend/, $self->get_tracks;
+}
+
+sub init_cell_line {
+  my $self = shift;
+  my (%on, $i);
+  
+  $_->remove for grep !$self->{'reg_feats_tracks'}{$_->id}, $self->get_tracks;
+  
+  $on{$_->data->{'cell_line'}} ||= [ $_, $i++ ] for grep $_->get('display') ne 'off', $self->get_tracks;
+  
+  foreach (grep $_->[1], values %on) {
+    my $spacer = $_->[0]->before($self->create_track("spacer_$_->[1]", '', { glyphset => 'spacer', strand => 'r', colour => 'grey40', width => $self->image_width, height => 2 }));
+    my $after  = $_->[0]->get('track_after');
+    
+    if ($after) {
+      $spacer->set('track_after', $after);
+      $_->[0]->set('track_after', $spacer);
+    }
+  }
+  
+  $self->add_tracks('other',
+    [ 'draggable', '', 'draggable', { display => 'normal', strand => 'b', menu => 'no' }]
+  );
+}
+
+sub init_bottom {
+  my $self = shift;
+  
+  $_->remove for grep $_->id !~ /_legend/, $self->get_tracks;
+
+  $self->add_tracks('other',
+    [ 'fg_background_regulation', '', 'fg_background_regulation', { display => 'normal', strand => 'r', menu => 'no', tag => 0            }],
+    [ 'scalebar',                 '', 'scalebar',                 { display => 'normal', strand => 'r', menu => 'no', name => 'Scale bar' }],
+    [ 'ruler',                    '', 'ruler',                    { display => 'normal', strand => 'r', menu => 'no', name => 'Ruler'     }],
+  );
+}
+
+1;

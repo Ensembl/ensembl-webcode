384fa2303eaf5fc5c0729cbfac5639a10ebdb587
Steve Trevanion <st3@sanger.ac.uk>
2013-04-16 14:33:31 +0000

    connect to locally configured session database if available (is for mirrors)

--- utils/clean_mart_sessions_table.pl
+++ utils/clean_mart_sessions_table.pl
@@ -19,11 +19,8 @@ BEGIN {
 
 my $hub = new EnsEMBL::Web::Hub;
 my $sd  = $hub->species_defs;
-
-my $dbh = DBI->connect(
-  sprintf('DBI:mysql:database=%s;host=%s;port=%s', $sd->ENSEMBL_USERDB_NAME, $sd->ENSEMBL_USERDB_HOST, $sd->ENSEMBL_USERDB_PORT),
-  $sd->ENSEMBL_USERDB_USER, $sd->ENSEMBL_USERDB_PASS
-);
+my $dsn = sprintf('DBI:mysql:database=%s;host=%s;port=%s', $sd->ENSEMBL_USERDB_NAME, $sd->ENSEMBL_USERDB_HOST, $sd->multidb->{'DATABASE_SESSION'}{'PORT'} || $sd->ENSEMBL_USERDB_PORT);
+my $dbh = DBI->connect($dsn, $sd->ENSEMBL_USERDB_USER, $sd->ENSEMBL_USERDB_PASS);
 
 $dbh->do('DELETE from sessions WHERE modified_at < DATE(NOW()) - INTERVAL 1 WEEK');
 
@@ -31,5 +28,5 @@ $dbh->do('DELETE from sessions WHERE modified_at < DATE(NOW()) - INTERVAL 1 WEEK
 if (!(gmtime)[6]) {
   $dbh->do('OPTIMIZE TABLE sessions');
 }
-  
+
 $dbh->disconnect;

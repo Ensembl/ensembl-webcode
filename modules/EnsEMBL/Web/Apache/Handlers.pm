e3e14654e3cf7141ab3218353ed5f481d0efd817
Anne Parker <ap5@sanger.ac.uk>
2013-09-16 10:35:07 +0000

    Added code to handle species-less Gene/Summary URLs from ChEMBL

--- modules/EnsEMBL/Web/Apache/Handlers.pm
+++ modules/EnsEMBL/Web/Apache/Handlers.pm
@@ -295,11 +295,20 @@ sub handler {
   
   ## Identify the species element, if any
   my ($species, @path_segments);
- 
-  ## Check for stable id URL (/id/ENSG000000nnnnnn)
-  if ($raw_path[0] && $raw_path[0] =~ /^id$/i && $raw_path[1]) {
-    my $stable_id = $raw_path[1];
-    my ($object_type, $db_type, $uri);
+
+  ## Check for stable id URL (/id/ENSG000000nnnnnn) 
+  ## and malformed Gene/Summary URLs from external users
+  if (($raw_path[0] && $raw_path[0] =~ /^id$/i && $raw_path[1])
+    || ($raw_path[0] eq 'Gene' && $querystring =~ /g=/ ))
+  {
+    my ($stable_id, $object_type, $db_type, $uri);
+    if ($raw_path[0] =~ /^id$/i) {
+      $stable_id = $raw_path[1];
+    }
+    else {
+      $querystring =~ /g=(\w+)/;
+      $stable_id = $1;
+    }
     my $unstripped_stable_id = $stable_id;
     if ($stable_id =~ /^ENS/) {
 	    $stable_id =~ s/\.[0-9]+$//; ## Remove versioning for Ensembl ids

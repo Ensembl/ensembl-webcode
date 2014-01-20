962b623af0f0f832a2bcc503b1cad995ebf8ec0f
Dan Sheppard <ds23@sanger.ac.uk>
2014-01-20 11:40:44 +0000

    Initial decoder work.

--- htdocs/components/15_TextSequence.js
+++ htdocs/components/15_TextSequence.js
@@ -20,6 +20,7 @@ Ensembl.Panel.TextSequence = Ensembl.Panel.Content.extend({
     
     Ensembl.EventManager.register('dataTableRedraw', this, this.initPopups);
     Ensembl.EventManager.register('ajaxComplete',    this, this.sequenceKey);
+    Ensembl.EventManager.register('ajaxLoaded',      this, this.panelLoaded);
     Ensembl.EventManager.register('getSequenceKey',  this, this.getSequenceKey);
   },
   
@@ -93,5 +94,126 @@ Ensembl.Panel.TextSequence = Ensembl.Panel.Content.extend({
   getSequenceKey: function () {
     Ensembl.EventManager.unregister('ajaxComplete', this);
     return JSON.parse($('.sequence_key_json', this.el).html() || false);
+  },
+
+  expandStretch: function(s) {
+    var out = [],c,repl,newest=-1,recent=[],i,j,nr;
+    var digit = /^\d$/;
+
+    var repl = {
+      'Z': '>>',
+      'Y': '>b',
+      'X': 'VVVVV',
+      'W': 'UUUUU',
+      'V': 'TTT',
+      'U': 'QQQ',
+      'T': 'aaa',
+      'S': 'aaaa',
+      'R': 'aaaaa',
+      'Q': 'aaaaaaaaaa',
+      'G': 'FFFFF',
+      'F': 'EEEEE',
+      'E': 'DDD',
+      'D': 'CCC',
+      'C': 'AAA',
+      'B': '>>>>>',
+      'A': '>>>>>>>>>>'
+    }
+    
+    while(s.length) {
+      c = s.charAt(0);
+      s = s.substr(1);
+      if(c in repl) {
+        s = repl[c]+s;
+        continue;
+      }
+      if(c == '>') {
+        i = ++newest;
+      } else if(c == '[') {
+        i = 0;
+        while(digit.test(s.charAt(0))) {
+          i = i*10+(s.charCodeAt(0)-'0'.charCodeAt(0));
+          s = s.substr(1);
+        }
+        newest = i;
+      } else {
+        i = recent[c.charCodeAt(0)-'a'.charCodeAt(0)];
+      }
+      out.push(i);
+      nr = [i];
+      for(j=0;j<recent.length && j<24;j++) {
+        if(recent[j] != i) {
+          nr.push(recent[j]);
+        }
+      }
+      recent = nr; 
+
+    }
+    return out;
+  },
+
+  buildKeys: function(data) {
+    var j,out = [];
+    var prefixes = this.expandStretch(data.lengthstr);
+    var prev = "";
+
+    for(j=0;j<prefixes.length && j<data.suffixes.length;j++) {
+      var str = prev.substr(0,data.lengths[prefixes[j]])+data.suffixes[j];
+      out.push(str)
+      prev = str;
+    }
+    return out;
+  },
+
+  markupSequence: function(data,$el,i) {
+    var panel = this;
+    var j,states,p,empty,empty_state=-1,statestart=-1,values,seq,newstate;
+
+    values = [];
+    seq = [];
+    $.each(data.keys,function(k,v) {
+      values[k] = panel.buildKeys(v);
+      seq[k] = panel.expandStretch(v.main);
+    });
+
+    states = this.expandStretch(data.main);
+    states.push({});
+    p = -1;
+    for(j=0;j<states.length;j++) {
+      if(states[j] == empty_state) {
+        1; //
+      } else if(states[j] != p) {
+        empty = 1;
+        $.each(data.keys,function(k,v) {
+          if(values[k][seq[k][states[j]]]) { empty = 0; } 
+        });
+        if(empty) { empty_state = states[j]; }
+        p = states[j];
+      }
+      if(states[j] == empty_state && statestart != -1) {
+        newstate = {}
+        $.each(data.keys,function(k,v) {
+          newstate[k] = values[k][seq[k][states[statestart]]];
+        });
+        console.log(JSON.stringify(newstate),statestart,j);
+        statestart = -1;
+      } else if(states[j] != empty_state && statestart == -1) {
+        statestart = j;
+      }
+    }
+  },
+
+  panelLoaded: function() {
+    var panel = this;
+
+    Ensembl.EventManager.unregister('ajaxLoaded', this);
+    $('.stretch_data',this.el).each(function() {
+      var i;
+      var data = $.parseJSON($(this).text());
+      for(i=0;i<data.length;i++) {
+        panel.markupSequence(data[i],$(this),i);
+      }
+    });
   }
+
 });

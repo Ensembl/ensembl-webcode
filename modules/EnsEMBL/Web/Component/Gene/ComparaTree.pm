454ad79b4c686c592b815c42bba406085baa5ba8
Anne Lyle <annelyle@ebi.ac.uk>
2016-02-18 09:41:26 +0000

    Pass additional parameters through to export
    Conflicts:
    
    	modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
    	modules/EnsEMBL/Web/Component/ImageExport/ImageFormats.pm

--- modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
+++ modules/EnsEMBL/Web/Component/Gene/ComparaTree.pm
@@ -319,7 +319,19 @@ sub content {
   else {
     $gene_name = $tree_stable_id;
   }
-  $image->{'export_params'} = [['gene_name', $gene_name],['align', 'tree'], ['cdb', $cdb]];
+
+  ## Parameters to pass into export form
+  $image->{'export_params'} = [['gene_name', $gene_name],['align', 'tree']];
+  my @extra_params = qw(g1 anc cdb collapse);
+  foreach (@extra_params) {
+    push @{$image->{'export_params'}}, [$_, $hub->param($_)];
+  }
+  foreach ($hub->param) {
+    if (/^group/) {
+      push @{$image->{'export_params'}}, [$_, $hub->param($_)];
+    }
+  }
+
   $image->{'data_export'}   = 'GeneTree';
   $image->{'remove_reset'}  = 1;
 

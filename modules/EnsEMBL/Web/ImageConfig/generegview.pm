bc9452a21327b0a198241b0a4332ab2e137bb6a7
Dan Sheppard <dan@ebi.ac.uk>
2016-08-12 12:15:21 +0100

    All GTEX data now present.

--- modules/EnsEMBL/Web/ImageConfig/generegview.pm
+++ modules/EnsEMBL/Web/ImageConfig/generegview.pm
@@ -65,11 +65,7 @@ sub init {
     ],
   }) if($gencode_version);
 
-  my @gtex_tissues = sort keys %{$self->hub->species_defs->REST_gtex_tissues||{}};
-
-  # EXCLUDED TISSUES FOR FIRST WEEK OF 85.
-  @gtex_tissues = grep { !/^[M-Z]/ } @gtex_tissues;
-
+  # Should be configured by regulation  
   my $gtex_tissue_example = "Whole_Blood";
   unless(any { $_ eq $gtex_tissue_example } @gtex_tissues) {
     $gtex_tissue_example = $gtex_tissues[0];

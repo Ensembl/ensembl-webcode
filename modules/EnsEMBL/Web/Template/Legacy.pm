32d9941c09da47a8aa5c971898e768f127d78581
Jyothish <joyjoy.nt@gmail.com>
2018-05-15 16:10:57 +0100

    Show cookie banner only if gdpr accounts version is available

--- modules/EnsEMBL/Web/Template/Legacy.pm
+++ modules/EnsEMBL/Web/Template/Legacy.pm
@@ -200,6 +200,7 @@ sub render_page_end {
 
   ## JAVASCRIPT AND OTHER STUFF THAT NEEDS TO BE HIDDEN AT BOTTOM OF PAGE
   my $species_path        = $hub->species_defs->species_path;
+  my $gdpr_version        = $hub->species_defs->GDPR_ACCOUNTS_VERSION;
   my $species_common_name = $hub->species_defs->SPECIES_COMMON_NAME;
   my $max_region_length   = 1000100 * ($hub->species_defs->ENSEMBL_GENOME_SIZE || 1);
   my $core_params         = $hub->core_params || {};
@@ -215,6 +216,7 @@ sub render_page_end {
     <fieldset>$core_params_html</fieldset>
   </form>
   <input type="hidden" id="species_path" name="species_path" value="$species_path" />
+  <input type="hidden" id="gdpr_version" name="gdpr_version" value="$gdpr_version" />
   <input type="hidden" id="species_common_name" name="species_common_name" value="$species_common_name" />
   <input type="hidden" id="max_region_length" name="max_region_length" value="$max_region_length" />
     $elements->{'modal'}

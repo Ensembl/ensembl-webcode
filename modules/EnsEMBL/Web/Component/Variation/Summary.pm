4e49fa5f88d2308afaaaecf756018c48f7818466
Sarah Hunt <seh@ebi.ac.uk>
2021-10-21 17:39:12 +0100

    fix source

--- modules/EnsEMBL/Web/Component/Variation/Summary.pm
+++ modules/EnsEMBL/Web/Component/Variation/Summary.pm
@@ -223,7 +223,7 @@ sub variation_source {
       $source_link = "";
     } 
   } elsif ($source eq "EVA") {
-    $source_link = $hub->get_ExtURL_link("$source_prefix $source", $sname, $name);
+    $source_link = $hub->get_ExtURL_link("$source_prefix $source", $source, $name);
   } elsif ($source =~ /ClinVar/i) {
     $sname = ($name =~ /^rs/) ?  'CLINVAR_DBSNP' : 'CLINVAR';
     $source_link = $hub->get_ExtURL_link("About $source", $sname, $name);

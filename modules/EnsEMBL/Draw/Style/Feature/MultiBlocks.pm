5fce478daeabe7043b7b7dd6a399843bad2a1671
Imran Salam <imran@ebi.ac.uk>
2022-07-27 13:32:57 +0000

    fix the regulation tracks width

--- modules/EnsEMBL/Draw/Style/Feature/MultiBlocks.pm
+++ modules/EnsEMBL/Draw/Style/Feature/MultiBlocks.pm
@@ -60,7 +60,7 @@ sub draw_feature {
   my $x                     = $feature->{'start'} - 1;
   $x                        = 0 if $x < 0;
   $params{'x'}              = $x;
-  $params{'width'}          = $position->{'width'};
+  $params{'width'}          = $position->{'width'} - 1;
   $params{'colour'}         = $feature->{'colour'};
   $params{'bordercolour'}   = $feature->{'bordercolour'};
   $params{'pattern'}        = $feature->{'pattern'};

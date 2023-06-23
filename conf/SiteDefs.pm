68d5fb69f4d856f75835d4c309043e028982ba49
John Tate <jgtate@gmail.com>
2023-06-23 16:21:59 +0000

    Update release date

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -35,8 +35,8 @@ use Sys::Hostname::Long;
 ###############################################################################
 ## Ensembl Version and release dates (these get updated every release)
 our $ENSEMBL_VERSION        = 110;            # Ensembl release number
-our $ARCHIVE_VERSION        = 'May2023';     # Archive site for this version
-our $ENSEMBL_RELEASE_DATE   = 'May 2023'; # As it would appear in the copyright/footer
+our $ARCHIVE_VERSION        = 'Jul2023';     # Archive site for this version
+our $ENSEMBL_RELEASE_DATE   = 'July 2023'; # As it would appear in the copyright/footer
 ###############################################################################
 
 

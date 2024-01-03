155b9c6f95640dc2d3a11e794702bd1a9eda2f25
Jyothish <joyjoy.nt@gmail.com>
2024-01-03 12:03:28 +0000

    Update release date and archive version

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -35,8 +35,8 @@ use Sys::Hostname::Long;
 ###############################################################################
 ## Ensembl Version and release dates (these get updated every release)
 our $ENSEMBL_VERSION        = 111;            # Ensembl release number
-our $ARCHIVE_VERSION        = 'Nov2023';     # Archive site for this version
-our $ENSEMBL_RELEASE_DATE   = 'November 2023'; # As it would appear in the copyright/footer
+our $ARCHIVE_VERSION        = 'Jan2024';     # Archive site for this version
+our $ENSEMBL_RELEASE_DATE   = 'January 2024'; # As it would appear in the copyright/footer
 ###############################################################################
 
 

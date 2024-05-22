0877f0a8135ba4a33b72965dff315046af54c7b7
Imran Salam <imran@ebi.ac.uk>
2024-05-22 10:53:58 +0000

    Update release date for 113

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -34,9 +34,9 @@ use Sys::Hostname::Long;
 
 ###############################################################################
 ## Ensembl Version and release dates (these get updated every release)
-our $ENSEMBL_VERSION        = 112;            # Ensembl release number
-our $ARCHIVE_VERSION        = 'May2024';     # Archive site for this version
-our $ENSEMBL_RELEASE_DATE   = 'May 2024'; # As it would appear in the copyright/footer
+our $ENSEMBL_VERSION        = 113;            # Ensembl release number
+our $ARCHIVE_VERSION        = 'Sep2024';     # Archive site for this version
+our $ENSEMBL_RELEASE_DATE   = 'September 2024'; # As it would appear in the copyright/footer
 ###############################################################################
 
 

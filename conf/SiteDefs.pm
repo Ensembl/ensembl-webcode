288d9f4996a8d5ade7b92d2cd89c8f38b2f1e2f8
Tamara El Naboulsi <48948777+TamaraNaboulsi@users.noreply.github.com>
2026-03-09 14:01:03 +0000

    Update release date

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -35,8 +35,8 @@ use Sys::Hostname::Long;
 ###############################################################################
 ## Ensembl Version and release dates (these get updated every release)
 our $ENSEMBL_VERSION        = 116;            # Ensembl release number
-our $ARCHIVE_VERSION        = 'Apr2026';     # Archive site for this version
-our $ENSEMBL_RELEASE_DATE   = 'April 2026'; # As it would appear in the copyright/footer
+our $ARCHIVE_VERSION        = 'May2026';     # Archive site for this version
+our $ENSEMBL_RELEASE_DATE   = 'May 2026'; # As it would appear in the copyright/footer
 ###############################################################################
 
 
